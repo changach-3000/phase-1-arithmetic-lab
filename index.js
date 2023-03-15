83c123d898f2518d3b7575dac2daf6e56e9ed245
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-15 21:23:48 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 *num2;
+ 
+// test 2
+let random = Math.floor((Math.random()) + 1);
+
+//test 3
+let num3 = 5;
+let num4 = 29;
+
+const mod = num4 % num3;
+
+// test 4
+let max =Math.max(13,12,20);
